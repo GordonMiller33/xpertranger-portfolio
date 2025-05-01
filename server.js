1976bc4ed209e9411e0b8dd74cc84619b4d51132
Gordon <gordonmiller33@gmail.com>
2025-04-30 22:50:03 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
