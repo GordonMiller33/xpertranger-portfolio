7934cd5caad498be633a350510069cd0841891b7
Gordon <gordonmiller33@gmail.com>
2025-04-30 23:08:14 -0400

    added get command to server.js

--- server.js
+++ server.js
@@ -9,15 +9,32 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-// Serve static frontend files
 app.use(express.static('public'));
 
-// Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(() => console.log('MongoDB connected'))
   .catch(err => console.error(err));
+
+const brewSchema = new mongoose.Schema({
+  id: int,
+  title: String,
+  category: String,
+  summary: String,
+  content: String
+});
+
+const Brew = new mongoose.model('Brew', brewSchema);
+
+app.get('/brews', async(req,res) => {
+  try {
+    const brews = await Brew.find();
+    res.json(brews);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+})
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
