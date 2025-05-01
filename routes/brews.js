d32eeb637006e92ba4e7310ff39c5de119a4a496
Gordon <gordonmiller33@gmail.com>
2025-05-01 01:07:51 -0400

    fixed server,js issues

--- routes/brews.js
+++ routes/brews.js
@@ -23,8 +23,4 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
 module.exports = router;
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const brewsRoute = require('./routes/brews');
+const path = require('path');
 require('dotenv').config();
 
 const app = express();
@@ -14,16 +15,25 @@ app.use(express.static('public'));
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-}).then(() => console.log('MongoDB connected'))
+}).then(() => {
+  console.log('MongoDB connected')
+  startServer();
+})
   .catch(err => console.error(err));
 
 app.use('/brews', brewsRoute);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ message: 'Something went wrong!' });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+function startServer() {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
