0cadfb6429bfa0efc900e44c09cb48bac6f1e7c1
Gordon <gordonmiller33@gmail.com>
2025-05-25 08:41:03 -0400

    Setup login page and basic admin middleware

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const path = require("path");
 
-const brewsRouter = require("./routes/brews-route");	//imports a router for handling brew requests
+const routesPath = path.join(__dirname, "routes")
+
+const brewsRouter = require(path.join(routesPath, "brews-route"));	//imports a router for handling brew requests
 
 require("dotenv").config();	//sets up .env file config to be accessed via process.env.<value>
 
@@ -15,6 +17,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/admin', express.static(path.join(__dirname, 'login')));
+
 mongoose.set('strictQuery', false);
 
 mongoose.connect(process.env.MONGODB_URI, {
@@ -22,7 +26,7 @@ mongoose.connect(process.env.MONGODB_URI, {
   	useUnifiedTopology: true,
 }).then(() => {
 	app.listen(PORT, () => {
-		console.log(`Server running on port`, PORT);
+		console.log(`Server running on the port`, PORT);
 	});
 }).catch(err => console.log(err));
 
